d2084f846f6c081f95a6377dbf879bf7dfe8d013
Daniel Wajsman <danielwajsman@mac.com>
2022-07-01 17:54:41 -0700

    menu items + Firebase install

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './routes/home/home.component.jsx'
+import Navigation from "./routes/navigation/navigation.component.jsx";
+import SignIn from "./routes/sign-in/sign-in.component"
+
+const  Shop = () => {
+  return (
+    <p>SHOPPPPP</p>
+  )
+}
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+
+
+      </Route>
+
     </Routes>
   );
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom';
+
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+import './navigation.styles.scss'
+
+export default function navigation() {
+  return (
+    <div>
+    <div className='navigation'>
+      <Link className='logo-container' to='/'>
+        <Logo />
+      </Link>
+      <div className='nav-links-container'>
+        <Link className='nav-link' to="/shop">
+          SHOP
+        </Link>
+        <Link className='nav-link' to="/sign-in">
+          SIGN IN
+        </Link>
+      </div>
+    </div>
+
+      <Outlet />
+      </div>
+  )
+}
